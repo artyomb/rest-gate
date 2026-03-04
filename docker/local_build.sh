3fe5264b3b38dcd7a4cb2274ebe4fa6a8d3c53fa
AlexanderKuznetsov <kyzalexandr@yandex.ru>
2026-03-04 10:47:47 +0300

    Initial commit

---
+++ docker/local_build.sh
@@ -0,0 +1,11 @@
+CI_PROJECT_NAME=rest_gate
+export CI_PIPELINE_ID=local
+export CI_PIPELINE_IID=0
+export CI_REGISTRY_HOST=localhost
+export CI_SKIP_PUSH=true
+
+#  -v /root/.docker:/root/.docker \
+docker run --rm --env-file <(env | grep ^CI_) \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  $(docker build -q -t build/${CI_PROJECT_NAME} -f Dockerfile.build ..) 2>&1
+
